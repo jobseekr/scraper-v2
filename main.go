048fc3aee5fb1020bfcf0e8cc773fc70fc950931
Kanisk <57884127+chakrakan@users.noreply.github.com>
2020-12-26 07:26:18 -0500

    add functionality to get jobs across pages

--- main.go
+++ main.go
@@ -4,17 +4,77 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
+	"strings"
 
 	"github.com/PuerkitoBio/goquery"
 )
 
-var baseURL string = "https://ca.indeed.com/jobs?q=Software+Developer&l=Toronto%2C+ON&sort=date"
-
-func main() {
-	getPages()
+type extractedJob struct {
+	id       string
+	location string
+	title    string
+	salary   string
+	summary  string
 }
 
-func getPages() {
+var baseURL string = "https://ca.indeed.com/jobs?q=Software+Developer&l=Toronto%2C+ON&radius=100&limit=50&fromage=7&sort=date"
+
+func main() {
+	var jobs []extractedJob
+	totalPages := getPages()
+
+	for i := 0; i < totalPages; i++ {
+		extractedJobs := getPage(i)
+		jobs = append(jobs, extractedJobs...)
+	}
+
+	fmt.Println(jobs)
+}
+
+func getPage(pageNum int) []extractedJob {
+	var jobs []extractedJob
+	pageURL := baseURL + "&start=" + strconv.Itoa(pageNum*50)
+	fmt.Println("Requesting ", pageURL)
+	res, err := http.Get(pageURL)
+	checkErr(err)
+	checkCode(res)
+
+	defer res.Body.Close()
+
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	checkErr(err)
+
+	searchCards := doc.Find(".jobsearch-SerpJobCard")
+
+	searchCards.Each(func(i int, card *goquery.Selection) {
+		job := extractJob(card)
+		jobs = append(jobs, job)
+	})
+	return jobs
+}
+
+func extractJob(card *goquery.Selection) extractedJob {
+	id, _ := card.Attr("data-jk")
+	title := cleanString(card.Find(".title>a").Text())
+	location := cleanString(card.Find(".sjcl").Text())
+	salary := cleanString(card.Find(".salaryText").Text())
+	summary := cleanString(card.Find(".summary").Text())
+	return extractedJob{
+		id:       id,
+		title:    title,
+		location: location,
+		salary:   salary,
+		summary:  summary,
+	}
+}
+
+func cleanString(str string) string {
+	return strings.Join(strings.Fields(strings.TrimSpace(str)), " ")
+}
+
+func getPages() int {
+	pages := 0
 	res, err := http.Get(baseURL)
 	checkErr(err)
 	checkCode(res)
@@ -25,8 +85,9 @@ func getPages() {
 	checkErr(err)
 
 	doc.Find(".pagination-list").Each(func(i int, s *goquery.Selection) {
-		fmt.Println(s.Find("a").Length())
+		pages = s.Find("a").Length()
 	})
+	return pages
 }
 
 func checkErr(err error) {
